f08583c19f54eb04ff06fd3067b38c43e25fc870
MaheshaDinu98 <maheeshadinushan@gmail.com>
2025-07-29 13:16:45 +0530

    enebled to get password when checking credentials

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -7,15 +7,14 @@ import User from "../models/user.model";
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
-const REFRESH_SECRET = process.env.REFRESH_SECRET as string;
+const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET as string;
 
 const refreshTokens = new Set<string>();
 
 export const authenticateUser = async (email: string, password: string) => {
-    const existingUser: UserDto | null =  await User.findOne({email: email});
+    const existingUser: UserDto | null =  await User.findOne({email: email}).select("+password");
 
-    let isValidPassword = undefined != existingUser
-        && bcrypt.compareSync(password, existingUser.password);
+    let isValidPassword = undefined != existingUser && bcrypt.compareSync(password, existingUser.password);
     if (!existingUser || !isValidPassword) {
         return null;
     }
