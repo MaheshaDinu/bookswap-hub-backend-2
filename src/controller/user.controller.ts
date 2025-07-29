1f9a979b1c7515087971a966990f9345fbd5dd65
MaheshaDinu98 <maheeshadinushan@gmail.com>
2025-07-29 10:48:42 +0530

    user.controller.ts implemented with async and await

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,14 +1,14 @@
 import {Request, Response} from "express";
 import * as userService from "../service/user.service";
 
-export const createUser = (req: Request, res: Response) => {
+export const createUser = async (req: Request, res: Response) => {
     try {
         const newUser = req.body;
-        const validationError = userService.validateUser(newUser, true); // true for new user validation
+        const validationError = await userService.validateUser(newUser, true); // true for new user validation
         if (validationError) {
             return res.status(400).json({ message: validationError });
         }
-        const createdUser = userService.createUser(newUser);
+        const createdUser = await userService.createUser(newUser);
         res.status(201).json(createdUser);
     } catch (error) {
         if (error instanceof Error) {
@@ -20,9 +20,9 @@ export const createUser = (req: Request, res: Response) => {
     }
 };
 
-export const getAllUsers = (req: Request, res: Response) => {
+export const getAllUsers = async (req: Request, res: Response) => {
     try {
-        const users = userService.getAllUsers();
+        const users = await userService.getAllUsers();
         res.status(200).json(users);
     } catch (error) {
         if (error instanceof Error) {
@@ -34,13 +34,13 @@ export const getAllUsers = (req: Request, res: Response) => {
     }
 };
 
-export const getUserById = (req: Request, res: Response) => {
+export const getUserById = async (req: Request, res: Response) => {
     try {
         const userId = parseInt(req.params.id);
         if (isNaN(userId)) { // Check if ID is a valid number
             return res.status(400).json({ message: "Invalid User ID." });
         }
-        const user = userService.getUserById(userId);
+        const user = await userService.getUserById(userId);
         if (!user) {
             return res.status(404).json({ message: "User not found." });
         }
@@ -55,18 +55,18 @@ export const getUserById = (req: Request, res: Response) => {
     }
 };
 
-export const updateUser = (req: Request, res: Response) => {
+export const updateUser = async (req: Request, res: Response) => {
     try {
         const userId = parseInt(req.params.id);
         if (isNaN(userId)) {
             return res.status(400).json({ message: "Invalid User ID." });
         }
         const updatedUserData = req.body;
-        const validationError = userService.validateUser(updatedUserData, false, userId); // false for update validation
+        const validationError = await userService.validateUser(updatedUserData, false, userId); // false for update validation
         if (validationError) {
             return res.status(400).json({ message: validationError });
         }
-        const updatedUser = userService.updateUser(userId, updatedUserData);
+        const updatedUser = await userService.updateUser(userId, updatedUserData);
         if (!updatedUser) {
             return res.status(404).json({ message: "User not found." });
         }
@@ -81,13 +81,13 @@ export const updateUser = (req: Request, res: Response) => {
     }
 };
 
-export const deleteUser = (req: Request, res: Response) => {
+export const deleteUser = async (req: Request, res: Response) => {
     try {
         const userId = parseInt(req.params.id);
         if (isNaN(userId)) {
             return res.status(400).json({ message: "Invalid User ID." });
         }
-        const deletedUser = userService.deleteUser(userId);
+        const deletedUser =await userService.deleteUser(userId);
         if (!deletedUser) {
             return res.status(404).json({ message: "User not found." });
         }
