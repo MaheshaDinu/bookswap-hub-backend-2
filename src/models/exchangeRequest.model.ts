1e1b44fb93561e5122f72954c8e233eb8a5c3e65
MaheshaDinu98 <maheeshadinushan@gmail.com>
2025-07-29 10:54:07 +0530

    ExchangeRequest.dto.ts implemented and exchangeRequest.model.ts implemented with mongoose

--- src/models/exchangeRequest.model.ts
+++ src/models/exchangeRequest.model.ts
@@ -1,11 +1,40 @@
+import mongoose from "mongoose";
 import RequestStatus from "../enums/requestStatus";
 
-export interface ExchangeRequest{
-    id: number;
-    requester_id: number;
-    requested_book_id: number;
-    offered_book_id?: number;
-    status: RequestStatus;
-    createdAt: Date;
-    updatedAt: Date
-}
+const ExchangeRequestModel = new mongoose.Schema({
+    "id": {
+        required: true,
+        type: Number,
+        unique: true,
+        index: true,
+    },
+    "requester_id": {
+        required: true,
+        type: Number,
+    },
+    "receiver_id": {
+        required: true,
+        type: Number,
+    },
+    "book_id": {
+        required: true,
+        type: Number,
+    },
+    "status": {
+        required: true,
+        type: String,
+        enum: RequestStatus
+    },
+    "createdAt": {
+        required: true,
+        type: Date
+    },
+    "updatedAt": {
+        required: true,
+        type: Date,
+    }
+});
+
+const ExchangeRequest = mongoose.model("ExchangeRequest", ExchangeRequestModel);
+
+export default ExchangeRequest;
